a726eb161b3379d84273562329e6bf318db3b67a
luohang <122323600@qq.com>
2017-04-27 16:21:29 +0800

    base64

--- YLZViewer/src/com/ylz/controller/StudyController.java
+++ YLZViewer/src/com/ylz/controller/StudyController.java
@@ -1,6 +1,8 @@
 package com.ylz.controller;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -24,7 +26,6 @@ import com.ylz.common.mapper.SerieMapper;
 import com.ylz.common.mapper.StudyMapper;
 import com.ylz.dicom.dto.DicomImage;
 import com.ylz.dicom.service.DicomService;
-import com.ylz.util.Base64Util;
 import com.ylz.util.ConfigUtil;
 import com.ylz.util.JsonUtil;
 
@@ -66,11 +67,17 @@ public class StudyController {
 	@RequestMapping(value="/getStudy")
 	@ResponseBody
 	public Study getStudy(HttpServletRequest request,String imagePaths){
+		imagePaths="WyJDb25zdWx0LzIwMTcwNDI3LzIwMTcwMjIxMTQ0NjQxNDQ4OTAvMjAxNzA0MTAxMDQ2NDU0NzU0MC9DVC8yOTgzMjA1MV8yMDE3MDQyNzE0MDcxNzExOTQwIiwiQ29uc3VsdC8yMDE3MDQyNy8yMDE3MDIyMTE0NDY0MTQ0ODkwLzIwMTcwNDEwMTA0NjQ1NDc1NDAvQ1QvMjk4MzIwNjNfMjAxNzA0MjcxNDA3MTcyNzk1MCIsIkNvbnN1bHQvMjAxNzA0MjcvMjAxNzAyMjExNDQ2NDE0NDg5MC8yMDE3MDQxMDEwNDY0NTQ3NTQwL0NULzI5ODMyMDE1XzIwMTcwNDI3MTQwNzE3Njc3OTAiLCJDb25zdWx0LzIwMTcwNDI3LzIwMTcwMjIxMTQ0NjQxNDQ4OTAvMjAxNzA0MTAxMDQ2NDU0NzU0MC9DVC8yOTgzMjAyN18yMDE3MDQyNzE0MDcxNzUxMzIwIiwiQ29uc3VsdC8yMDE3MDQyNy8yMDE3MDIyMTE0NDY0MTQ0ODkwLzIwMTcwNDEwMTA0NjQ1NDc1NDAvQ1QvMjk4MzIwMzlfMjAxNzA0MjcxNDA3MTc2MDU2MCIsIkNvbnN1bHQvMjAxNzA0MjcvMjAxNzAyMjExNDQ2NDE0NDg5MC8yMDE3MDQxMDEwNDY0NTQ3NTQwL0NULzI5ODMyMDc1XzIwMTcwNDI3MTQwNzE3MzY0MjAiXQ\u003d\u003d";
 		//imagePaths = "[\"/Refer/20170316/2017022114464144890/2017031619535223610/EMR\"]";
 		//imagePaths = "[\"/Refer/20170316/2017022114464144890/2017031619535223610/EMR/0001_000000_20140724_082301_29719_0\"]";
 		Study study=new Study();
 		if(StringUtils.isNotBlank(imagePaths)){
-			imagePaths = Base64Util.encode(imagePaths);
+			try {
+				sun.misc.BASE64Decoder decoder = new sun.misc.BASE64Decoder();
+				imagePaths = new String(decoder.decodeBuffer(imagePaths),"utf-8");
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
 			//json字符串转list
 			List<String> list = JsonUtil.json2List(imagePaths, String.class);
 			String storePath = "";
